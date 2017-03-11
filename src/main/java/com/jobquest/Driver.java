b5f4e19c7e9eff9db1f0a789591e4620bf8598b9
ykeysergit <tangeroknight@gmail.com>
2017-03-10 23:53:30 -0600

    enable ssl

--- src/main/java/com/jobquest/Driver.java
+++ src/main/java/com/jobquest/Driver.java
@@ -13,8 +13,11 @@ import spark.template.thymeleaf.ThymeleafTemplateEngine;
 public class Driver {
 	
 	private static TemplateEngine engine;
+	private static final String keyStorePassword="password";
+	private static final String keyStoreLocation="src/main/config/security/jobquest.jks";
 	
 	public static void init() throws IOException{
+		secure(keyStoreLocation, keyStorePassword, null, null);
 		staticFiles.location("/public/app");
 		RouteOverview.enableRouteOverview("/routes");
 		engine=new ThymeleafTemplateEngine("public/app/templates/",".html");
