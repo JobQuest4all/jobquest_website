eed082e618f6f6fe8aa82a953b6a38979c4c3872
ykeysergit <tangeroknight@gmail.com>
2017-03-10 23:06:25 -0600

    stabilize server

--- src/main/java/com/jobquest/Driver.java
+++ src/main/java/com/jobquest/Driver.java
@@ -1,10 +1,34 @@
 package com.jobquest;
 
 import static spark.Spark.*;
+import spark.ModelAndView;
+import spark.TemplateEngine;
+
+import java.io.IOException;
+import java.util.*;
+
+import spark.route.RouteOverview;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
 
 public class Driver {
-    public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
-
+	
+	private static TemplateEngine engine;
+	
+	public static void init() throws IOException{
+		staticFiles.location("/public/app");
+		RouteOverview.enableRouteOverview("/routes");
+		engine=new ThymeleafTemplateEngine("public/app/templates/",".html");
+	}
+	
+	public static void defineRoutes(){
+		final ModelAndView map = new ModelAndView(new HashMap<String,String>(), "index");
+		
+		get("/", (req, resp) -> map, engine);
+		get("/jobquest", (req, resp) -> map, engine);
+	}
+	
+    public static void main(String[] args) throws IOException {
+        init();
+        defineRoutes();
     }
 }
